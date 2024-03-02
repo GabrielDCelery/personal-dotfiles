e25a9ac3382c867b9ac2f2f5f65fadcbb573187d
GaborZeller <gabriel.d.celery@gmail.com>
2024-03-02 14:21:41 +0000

    emoved WSL specific configuration

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,19 +10,10 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
--- config.color_scheme = 'AdventureTime'
-
-config.default_domain = 'WSL:Ubuntu'
-
 -- Automatically reload config
 config.automatically_reload_config = true
 
 -- Style
--- config.color_scheme = 'Default (dark) (terminal.sexy)'
-
 config.color_scheme = 'Dark Pastel (Gogh)'
 
 -- and finally, return the configuration to wezterm
