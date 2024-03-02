d7545adffbbd6670b2f7f760e0d5f409307f721b
GaborZeller <gabriel.d.celery@gmail.com>
2024-03-02 14:23:28 +0000

    Added wsl specific configuration

--- .wezterm.lua
+++ .wezterm.lua
@@ -16,5 +16,8 @@ config.automatically_reload_config = true
 -- Style
 config.color_scheme = 'Dark Pastel (Gogh)'
 
+-- WSL Ubuntu specific configuration
+config.default_domain = 'WSL:Ubuntu'
+
 -- and finally, return the configuration to wezterm
 return config
