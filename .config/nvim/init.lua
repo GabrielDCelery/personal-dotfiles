a7033b09c6fc17da44e5483d5266baa79f72fe5a
GaborZeller <gabriel.d.celery@gmail.com>
2024-06-02 23:00:39 +0100

    feat: Added neo-tree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -791,6 +791,17 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+  },
+
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
