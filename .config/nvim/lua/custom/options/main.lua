a7ab93d2fac5ea959bcae021518d4afa3deb7904
GaborZeller <gabriel.d.celery@gmail.com>
2024-08-17 08:41:25 +0100

    feat: set tabulation to 4 spaces

--- .config/nvim/lua/custom/options/main.lua
+++ .config/nvim/lua/custom/options/main.lua
@@ -68,3 +68,8 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
