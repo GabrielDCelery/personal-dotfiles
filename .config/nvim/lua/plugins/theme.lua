1f0fb22e9dee80bd68f706e77e3d60025d0fbecd
GaborZeller <gabriel.d.celery@gmail.com>
2024-08-24 12:49:09 +0100

    feat: changed themes from Macchiato to Mocha

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -2,7 +2,7 @@ return {
   'catppuccin/nvim',
   priority = 1000,
   init = function()
-    vim.cmd.colorscheme 'catppuccin-macchiato'
+    vim.cmd.colorscheme 'catppuccin-mocha'
     vim.cmd.hi 'Comment gui=none'
   end,
 }
