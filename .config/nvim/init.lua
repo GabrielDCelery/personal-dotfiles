17fdcde4db8152a8c1e090302804b23980ae212e
GaborZeller <gabriel.d.celery@gmail.com>
2024-09-15 21:56:02 +0100

    feat: added render markdown plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -57,6 +57,7 @@ require('lazy').setup({
   require 'plugins.neogit',
   require 'plugins.barbar',
   require 'plugins.autoclose',
+  require 'plugins.render-markdown',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
