81aea95949d21ea5fa6098764377303437a0785d
GaborZeller <gabriel.d.celery@gmail.com>
2024-09-18 22:14:27 +0100

    feat: added key bindings to barbar to close buffers

--- .config/nvim/lua/plugins/barbar.lua
+++ .config/nvim/lua/plugins/barbar.lua
@@ -1,3 +1,15 @@
+local function closeAllBuffersExceptCurrent()
+  local bufs = vim.api.nvim_list_bufs()
+  local current_buf = vim.api.nvim_get_current_buf()
+  for _, buf in ipairs(bufs) do
+    if buf ~= current_buf then
+      vim.api.nvim_buf_delete(buf, {})
+    end
+  end
+  -- Ensure we're still on the current buffer
+  vim.api.nvim_win_set_buf(vim.api.nvim_get_current_win(), current_buf)
+end
+
 return {
   'romgrk/barbar.nvim',
   dependencies = {
@@ -19,8 +31,12 @@ return {
   vim.keymap.set('n', '<leader>bn', '<cmd>BufferNext<cr>', { desc = 'BufferNext (barbar)' }),
   vim.keymap.set('n', '<S-Tab>', '<cmd>BufferPrevious<cr>', { desc = 'BufferPrevious (barbar)' }),
   vim.keymap.set('n', '<leader>bp', '<cmd>BufferPrevious<cr>', { desc = 'BufferPrevious (barbar)' }),
-  vim.keymap.set('n', '<leader>bc', '<cmd>BufferClose<CR>', { desc = 'BufferClose (barbar)' }),
+  vim.keymap.set('n', '<leader>bcc', '<cmd>BufferClose<CR>', { desc = 'BufferClose (barbar)' }),
   vim.keymap.set('n', '<leader>bmp', '<cmd>BufferMovePrevious<CR>', { desc = 'BufferMovePrevious (barbar)' }),
   vim.keymap.set('n', '<leader>bmn', '<cmd>BufferMoveNext<CR>', { desc = 'BufferMoveNext (barbar)' }),
   vim.keymap.set('n', '<leader>br', '<cmd>BufferRestore<CR>', { desc = 'BufferRestore (barbar)' }),
+  vim.keymap.set('n', '<leader>bco', '<cmd>BufferCloseAllButCurrent<CR>', { desc = 'BufferCloseAllButCurrent (barbar)' }),
+  vim.keymap.set('n', '<leader>bcr', '<cmd>BufferCloseBuffersRight<CR>', { desc = 'BufferCloseBuffersRight (barbar)' }),
+  vim.keymap.set('n', '<leader>bcl', '<cmd>BufferCloseBuffersLeft<CR>', { desc = 'BufferCloseBuffersLeft (barbar)' }),
+  -- vim.keymap.set('n', '<leader>bco', closeAllBuffersExceptCurrent, { desc = 'Close all other buffers' }),
 }
