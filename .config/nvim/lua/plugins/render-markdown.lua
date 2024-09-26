2ad201731412cde2fd37e9d889595828c2004068
GaborZeller <gabriel.d.celery@gmail.com>
2024-09-26 22:13:46 +0100

    feat: started customizing render markdown

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -7,4 +7,19 @@ return {
   }, -- if you use the mini.nvim suite
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  config = function()
+    require('render-markdown').setup {
+      heading = {
+        position = 'inline',
+        width = 'block',
+        border = true,
+        left_pad = 1,
+        right_pad = 3,
+      },
+      code = {
+        enabled = true,
+        width = 'block',
+      },
+    }
+  end,
 }
