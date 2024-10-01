90c037754c3ab7f760d51fccf272e214bd81d179
GaborZeller <gabriel.d.celery@gmail.com>
2024-10-01 21:44:24 +0100

    feat: added more key bindings to fugitive

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -4,6 +4,9 @@ return {
     vim.keymap.set('n', '<leader>gs', '<cmd>Git<cr>', { desc = 'Git status' })
     vim.keymap.set('n', '<leader>ga', '<cmd>Gwrite<cr>', { desc = 'Git add' })
     vim.keymap.set('n', '<leader>gb', '<cmd>Git blame<cr>', { desc = 'Git blame' })
+    vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<cr>', { desc = 'Git commit' })
+    vim.keymap.set('n', '<leader>gp', '<cmd>Git push<cr>', { desc = 'Git push' })
+    vim.keymap.set('n', '<leader>gP', '<cmd>Git pull<cr>', { desc = 'Git pull' })
     vim.keymap.set('n', '<leader>gl', '<cmd>Git log<cr>', { desc = 'Git log' })
     vim.keymap.set('n', '<leader>gd', '<cmd>Gdiffsplit<cr>', { desc = 'Git diff' })
   end,
