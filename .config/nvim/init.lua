d2b16fd280fd11c24a19575c1644c5c0d4999c48
GaborZeller <gabriel.d.celery@gmail.com>
2024-10-17 23:15:32 +0100

    feat: added flash

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -62,6 +62,7 @@ require('lazy').setup({
   require 'plugins.neogit',
   -- require 'plugins.fugitive',
   -- require 'plugins.lazygit',
+  require 'plugins.flash',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
