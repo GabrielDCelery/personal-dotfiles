58de7ea78d15f87fa08708ca823f16ef17058a0e
GaborZeller <gabriel.d.celery@gmail.com>
2024-10-20 16:13:17 +0100

    Added some key bindings for diffview

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -10,5 +10,9 @@ return {
   },
   config = true,
   vim.keymap.set('n', '<leader>gs', '<cmd>Neogit<cr>', { desc = 'NeoGit reveal' }),
+  vim.keymap.set('n', '<leader>gdo', '<cmd>DiffviewOpen<cr>', { desc = 'DiffviewOpen' }),
+  vim.keymap.set('n', '<leader>gdc', '<cmd>DiffviewClose<cr>', { desc = 'DiffviewClose' }),
+  vim.keymap.set('n', '<leader>gdr', '<cmd>DiffviewRefresh<cr>', { desc = 'DiffviewRefresh' }),
+  vim.keymap.set('n', '<leader>gdt', '<cmd>DiffviewToggleFiles<cr>', { desc = 'DiffviewToggleFiles' }),
   --  vim.keymap.set('n', '<leader>n', '<cmd>Neogit<cr>', { desc = 'NeoGit reveal' })
 }
