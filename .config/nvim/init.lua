f78530e215be78a0b9a642dd6c976d234df41456
GaborZeller <gabriel.d.celery@gmail.com>
2025-01-25 22:36:26 +0000

    feat: moved highlight on yank and lua installation to sub files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,29 +1,11 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
 require 'options.main'
 require 'options.keymap'
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.uv.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
+require('custom.highlight-when-yanking').init()
+require('custom.lazy-plugin-manager').init()
 
 require('lazy').setup({
   require 'plugins.theme',               -- configure and load theme for editor
