70f2aa3e297c8ee45659806dbf709148fb12dacc
GaborZeller <gabriel.d.celery@gmail.com>
2025-02-08 23:25:23 +0000

    feat: disabled swapping in textobjects

--- .config/nvim/lua/plugins/navigation/treesitter.lua
+++ .config/nvim/lua/plugins/navigation/treesitter.lua
@@ -72,10 +72,10 @@ return {
       swap = {
         enable = true,
         swap_next = {
-          ['<leader>a'] = '@parameter.inner',
+          -- ['<leader>a'] = '@parameter.inner',
         },
         swap_previous = {
-          ['<leader>A'] = '@parameter.inner',
+          -- ['<leader>A'] = '@parameter.inner',
         },
       },
       move = {
