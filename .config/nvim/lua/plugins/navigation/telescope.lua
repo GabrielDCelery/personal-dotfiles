ca49681527ccac15645e071e8a0f035a67b2035d
GaborZeller <gabriel.d.celery@gmail.com>
2025-03-17 22:01:40 +0000

    feat: added keymap for telescope searching git status

--- .config/nvim/lua/plugins/navigation/telescope.lua
+++ .config/nvim/lua/plugins/navigation/telescope.lua
@@ -84,6 +84,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     -- NOTE: The below need folke/todo-comments.nvim plugin to be installed
     vim.keymap.set('n', '<leader>st', '<cmd>TodoTelescope<cr>', { desc = 'search todo' })
     vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = 'search buffers' })
+    vim.keymap.set('n', '<leader>sG', builtin.git_status, { desc = 'search git status' })
     vim.keymap.set('n', '<leader>sn', function()
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
     end, { desc = 'search neovim' })
