e9b7880131bcb031bbfd539f74b3f89ed02695e4
GaborZeller <gabriel.d.celery@gmail.com>
2025-03-27 07:10:46 +0000

    feat: changed icons and colors for debugger

--- .config/nvim/lua/plugins/testing/debugger.lua
+++ .config/nvim/lua/plugins/testing/debugger.lua
@@ -12,49 +12,27 @@ return {
 
     vim.keymap.set('n', '<leader>dt', require('dapui').toggle, { noremap = true, desc = 'toggle debugger' })
     vim.keymap.set('n', '<leader>db', ':DapToggleBreakpoint<cr>', { noremap = true, desc = 'toggle breakpoint' })
-    vim.keymap.set('n', '<leader>dc', ':DapContinue', { noremap = true, desc = 'continue' })
+    vim.keymap.set('n', '<leader>dc', ':DapContinue<cr>', { noremap = true, desc = 'continue' })
     vim.keymap.set('n', '<leader>dr', function()
       require('dapui').open { reset = true }
     end, { noremap = true, desc = 'reset' })
-    vim.keymap.set('n', '<leader>dsi', ':DapStepInto<cr>', { noremap = true, desc = 'step into' })
-    vim.keymap.set('n', '<leader>dso', ':DapStepOut<cr>', { noremap = true, desc = 'step out' })
-    vim.keymap.set('n', '<leader>dsv', ':DapStepOver<cr>', { noremap = true, desc = 'step over' })
+    vim.keymap.set('n', '<leader>di', ':DapStepInto<cr>', { noremap = true, desc = 'step into' })
+    vim.keymap.set('n', '<leader>do', ':DapStepOut<cr>', { noremap = true, desc = 'step out' })
+    vim.keymap.set('n', '<leader>dv', ':DapStepOver<cr>', { noremap = true, desc = 'step over' })
 
+    vim.fn.sign_define('DapBreakpoint', { text = ' ', texthl = 'DapBreakpoint', linehl = '', numhl = '' })
     vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#f5c2e7' })
-    vim.api.nvim_set_hl(0, 'DapBreakpointCondition', { ctermbg = 0, fg = '#f5c2e7' })
-    vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#94e2d5' })
-    vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#fab387' })
-    vim.api.nvim_set_hl(0, 'DapBreakpointRejected', { ctermbg = 0, fg = '#f38ba8' })
 
-    vim.fn.sign_define('DapBreakpoint', {
-      text = ' ',
-      texthl = 'DapBreakpoint',
-      linehl = '',
-      numhl = '',
-    })
-    vim.fn.sign_define('DapBreakpointCondition', {
-      text = ' ',
-      texthl = 'DapBreakpointCondition',
-      linehl = '',
-      numhl = '',
-    })
-    vim.fn.sign_define('DapLogPoint', {
-      text = '󱙓 ',
-      texthl = 'DapLogPoint',
-      linehl = '',
-      numhl = '',
-    })
-    vim.fn.sign_define('DapStopped', {
-      text = ' ',
-      texthl = 'DapStopped',
-      linehl = 'DapStopped',
-      numhl = 'DapStopped',
-    })
-    vim.fn.sign_define('DapBreakpointRejected', {
-      text = ' ',
-      texthl = 'DapBreakpointRejected',
-      linehl = '',
-      numhl = '',
-    })
+    vim.fn.sign_define('DapBreakpointCondition', { text = ' ', texthl = 'DapBreakpointCondition', linehl = '', numhl = '' })
+    vim.api.nvim_set_hl(0, 'DapBreakpointCondition', { ctermbg = 0, fg = '#f5c2e7' })
+
+    vim.fn.sign_define('DapLogPoint', { text = '󱙓 ', texthl = 'DapLogPoint', linehl = '', numhl = '' })
+    vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#94e2d5' })
+
+    vim.fn.sign_define('DapStopped', { text = ' ', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
+    vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#fab387' })
+
+    vim.fn.sign_define('DapBreakpointRejected', { text = ' ', texthl = 'DapBreakpointRejected', linehl = '', numhl = '' })
+    vim.api.nvim_set_hl(0, 'DapBreakpointRejected', { ctermbg = 0, fg = '#f38ba8' })
   end,
 }
