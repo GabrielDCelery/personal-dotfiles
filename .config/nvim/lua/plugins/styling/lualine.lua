1b02c424a1a9d63644253b67b4e95330d4ac8ab0
GaborZeller <gabriel.d.celery@gmail.com>
2025-04-11 00:01:05 +0100

    feat: added code to show recording to register on lualine

--- .config/nvim/lua/plugins/styling/lualine.lua
+++ .config/nvim/lua/plugins/styling/lualine.lua
@@ -5,10 +5,26 @@ return {
     opt = true,
   },
   config = function()
+    local function recording_status()
+      local recording_register = vim.fn.reg_recording()
+      if recording_register == '' then
+        return ''
+      else
+        return ' Recording @' .. recording_register
+      end
+    end
     -- catppuccin is not controlled via the theme integrations but here
     require('lualine').setup {
       options = {
         theme = 'catppuccin',
+      },
+      sections = {
+        lualine_x = {
+          recording_status,
+          'encoding',
+          'fileformat',
+          'filetype',
+        },
       },
     }
   end,
