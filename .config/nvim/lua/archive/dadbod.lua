8440e301341e8a5537d6f2afc8b9db68659e9ad0
GaborZeller <gabriel.d.celery@gmail.com>
2025-04-12 00:22:25 +0100

    chore: removed redundant files from archive

--- .config/nvim/lua/archive/dadbod.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'tpope/vim-dadbod',
-  'kristijanhusak/vim-dadbod-completion',
-  'kristijanhusak/vim-dadbod-ui',
-  vim.keymap.set('n', '<leader>dt', '<cmd>DBUIToggle<CR>', { desc = 'Toggle DadBod' }),
-}
--- .config/nvim/lua/archive/lazygit.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'kdheepak/lazygit.nvim',
-  config = function()
-    vim.keymap.set('n', '<leader>g', '<cmd>LazyGit<cr>', { desc = 'Open LazyGit' })
-  end,
-}
