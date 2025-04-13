f4249c2fa7680a1a3441f9e8fb9d3b4c7b79b3a7
GaborZeller <gabriel.d.celery@gmail.com>
2025-04-13 14:16:26 +0100

    feat: added custom filetypes for managing helm files

---
+++ .config/nvim/lua/custom/filetype.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+-- add custom file types
+function M.init()
+  -- Add filetype detection for Helm templates
+  vim.filetype.add {
+    pattern = {
+      ['.*%.yaml%.tpl'] = 'helm',
+      ['.*%.yml%.tpl'] = 'helm',
+      ['.*%.helm%.yaml'] = 'helm',
+      ['.*%.helm%.yml'] = 'helm',
+      ['.*%/templates/.*%.yaml'] = 'helm',
+      ['.*%/templates/.*%.yml'] = 'helm',
+    },
+  }
+end
+
+return M
