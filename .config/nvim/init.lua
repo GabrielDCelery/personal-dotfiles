bd70157d2a1ffa349f95af352d3f2c5a71fba278
GaborZeller <gabriel.d.celery@gmail.com>
2025-05-27 16:31:43 +0100

    chore: ran autoformatting on init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,52 +8,52 @@ require 'options.keymap'
 
 -- Custom code snippets
 require('custom.highlight-when-yanking').init() -- highlight text when yanking
-require('custom.lazy-plugin-manager').init() -- the lazy plugin manager
+require('custom.lazy-plugin-manager').init()    -- the lazy plugin manager
 require('custom.autoformat-json-on-bufread').init()
-require('custom.filetype').init() -- custom filetypes
+require('custom.filetype').init()               -- custom filetypes
 
 require('lazy').setup({
   -- Neovim editor setup plugins
   require 'plugins.neovim.lazydev', -- auto configures the lua LSP for workspace libraries
 
   -- Theme/Styling
-  require 'plugins.styling.theme', -- configure and load theme for editor
-  require 'plugins.styling.noice', -- experimental plugin that replaces the command line and messages with a custom implementation
-  require 'plugins.styling.todo-commments', -- highlight todo comments in codebase (FIX, TODO, HACK, WARN, PERF, NOTE, TEST)
+  require 'plugins.styling.theme',           -- configure and load theme for editor
+  require 'plugins.styling.noice',           -- experimental plugin that replaces the command line and messages with a custom implementation
+  require 'plugins.styling.todo-commments',  -- highlight todo comments in codebase (FIX, TODO, HACK, WARN, PERF, NOTE, TEST)
   require 'plugins.styling.render-markdown', -- rendering markdown documents
-  require 'plugins.styling.lualine', -- editor status bar
-  require 'plugins.styling.which-key', -- displays key bindings
-  require 'plugins.styling.colorizer', -- color highlighter for RGB colors
-  require 'plugins.styling.web-devicons', -- nerd fonts for neovim plugins
+  require 'plugins.styling.lualine',         -- editor status bar
+  require 'plugins.styling.which-key',       -- displays key bindings
+  require 'plugins.styling.colorizer',       -- color highlighter for RGB colors
+  require 'plugins.styling.web-devicons',    -- nerd fonts for neovim plugins
 
   -- LSP/Formatting/Linting
-  require 'plugins.lsp.sleuth', -- auto adjust cursor to follow indentation
-  require 'plugins.lsp.lspconfig', -- language servers
-  require 'plugins.lsp.autoformat', -- autoformatter
+  require 'plugins.lsp.sleuth',         -- auto adjust cursor to follow indentation
+  require 'plugins.lsp.lspconfig',      -- language servers
+  require 'plugins.lsp.autoformat',     -- autoformatter
   -- require 'plugins.lsp.lint', -- linter
   require 'plugins.lsp.autocompletion', -- autocompletion
-  require 'plugins.lsp.autopairs', -- auto pair brackets and quotes
-  require 'plugins.lsp.spectre', -- search and replace text in the codebase
+  require 'plugins.lsp.autopairs',      -- auto pair brackets and quotes
+  require 'plugins.lsp.spectre',        -- search and replace text in the codebase
 
   -- Navigation/Searching
-  require 'plugins.navigation.flash', -- navigate within buffer jumping to characters
-  require 'plugins.navigation.neo-tree', -- browse and edit file system
-  require 'plugins.navigation.harpoon', -- harpoon for pinning buffers that keep revisiting
+  require 'plugins.navigation.flash',      -- navigate within buffer jumping to characters
+  require 'plugins.navigation.neo-tree',   -- browse and edit file system
+  require 'plugins.navigation.harpoon',    -- harpoon for pinning buffers that keep revisiting
   -- require 'plugins.navigation.telescope', -- telescope
   require 'plugins.navigation.treesitter', -- treesitter
-  require 'plugins.navigation.fzflua', -- versatile fzf based search
+  require 'plugins.navigation.fzflua',     -- versatile fzf based search
   -- require 'plugins.navigation.hydra', -- hydra
 
   -- Git/Version control
-  require 'plugins.git.neogit', -- built-in git GUI
+  require 'plugins.git.neogit',   -- built-in git GUI
   require 'plugins.git.gitsigns', -- git decorations
 
   -- Testing
-  require 'plugins.testing.vim-test', -- test initiator
+  require 'plugins.testing.vim-test',        -- test initiator
   require 'plugins.testing.better-quickfix', -- quickfix window
-  require 'plugins.testing.trouble', -- enhanced errors window
-  require 'plugins.testing.debugger', -- debugger
-  require 'plugins.testing.ai-assistant', -- ai assistant
+  require 'plugins.testing.trouble',         -- enhanced errors window
+  require 'plugins.testing.debugger',        -- debugger
+  require 'plugins.testing.ai-assistant',    -- ai assistant
 
   -- Terminal
   require 'plugins.terminal.toggleterm', -- inbuilt terminal
