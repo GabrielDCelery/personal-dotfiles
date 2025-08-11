cd64ef49ba307914fe676d5544a6345c10129587
GaborZeller <gabriel.d.celery@gmail.com>
2025-08-11 08:50:01 +0100

    feat: added key binding to jump between hunks to neovim

--- .config/nvim/lua/plugins/git/gitsigns.lua
+++ .config/nvim/lua/plugins/git/gitsigns.lua
@@ -23,6 +23,8 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
         -- map('n', '<leader>gp', gitsigns.preview_hunk_inline, { desc = 'Preview hunk' }) -- dont know if I need this
         -- map('n', '<leader>gtb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' }) -- prefer having blame turned on all the time
         map('n', '<leader>gtd', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' }) -- conflicting with toggle from neogit
+        map('n', ']g', '<cmd>Gitsigns next_hunk<cr>', { desc = 'Next hunk' })
+        map('n', '[g', '<cmd>Gitsigns prev_hunk<cr>', { desc = 'Previous hunk' })
         -- map('n', '<leader>gD', gitsigns.diffthis, { desc = 'git [d]iff against index' }) -- not important since we have neogit
       end,
     }
