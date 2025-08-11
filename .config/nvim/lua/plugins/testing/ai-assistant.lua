e765548f2682b8df7f3e17b0e525f9336bfbfc22
GaborZeller <gabriel.d.celery@gmail.com>
2025-08-11 23:57:59 +0100

    feat: downgraded claude 3.7 back to 3.5

--- .config/nvim/lua/plugins/testing/ai-assistant.lua
+++ .config/nvim/lua/plugins/testing/ai-assistant.lua
@@ -46,8 +46,8 @@ return {
     providers = {
       claude = {
         endpoint = 'https://api.anthropic.com',
-        -- model = 'claude-3-5-sonnet-20241022',
-        model = 'claude-3-7-sonnet-20250219',
+        model = 'claude-3-5-sonnet-20241022',
+        -- model = 'claude-3-7-sonnet-20250219',
         -- model = 'claude-sonnet-4-20250514',
         extra_request_body = {
           temperature = 0, -- adjust if needed
