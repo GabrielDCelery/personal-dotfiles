44668721b6c8aa37a00303483380171d8429f162
GaborZeller <gabriel.d.celery@gmail.com>
2025-10-08 18:40:53 +0100

    feat: enabled transparency for neovim

--- .config/nvim/lua/plugins/styling/theme.lua
+++ .config/nvim/lua/plugins/styling/theme.lua
@@ -2,12 +2,7 @@ return {
   'catppuccin/nvim',
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme 'catppuccin-mocha'
-    -- change the highligting behaviour
-    -- vim.cmd.gi / this is a Neovim command to set highlighting rules
-    -- Comment / refers to the syntax group of comments
-    -- gui=nonw / remove any special text formatting (e.g. italics) from comments
-    vim.cmd.hi 'Comment gui=none'
+    -- Configure catppuccin FIRST
     require('catppuccin').setup {
       default_integrations = true,
       -- !WARN: lualine is not controlled here via the theme integrations, but the lualine plugin itself
@@ -16,6 +11,20 @@ return {
         noice = true,
         harpoon = true,
       },
+      transparent_background = true,
+      float = {
+        transparent = true, -- enable transparent floating windows
+        solid = false, -- use solid styling for floating windows, see |winborder|
+      },
     }
+
+    -- THEN apply the colorscheme with the transparency settings
+    vim.cmd.colorscheme 'catppuccin-mocha'
+
+    -- change the highlighting behaviour
+    -- vim.cmd.hi / this is a Neovim command to set highlighting rules
+    -- Comment / refers to the syntax group of comments
+    -- gui=none / remove any special text formatting (e.g. italics) from comments
+    vim.cmd.hi 'Comment gui=none'
   end,
 }
