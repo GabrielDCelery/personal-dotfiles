9d21314a04b4aca60a9f903761a9a7e2164bbc72
GaborZeller <gabriel.d.celery@gmail.com>
2025-12-12 22:04:05 +0000

    fix: highlight on yank fix

--- .config/nvim/lua/custom/highlight-when-yanking.lua
+++ .config/nvim/lua/custom/highlight-when-yanking.lua
@@ -9,6 +9,7 @@ function M.init()
     group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
     callback = function()
       vim.highlight.on_yank()
+      -- vim.hl.on_yank()
     end,
   })
 end
