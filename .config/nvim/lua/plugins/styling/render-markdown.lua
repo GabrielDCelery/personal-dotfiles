d81031f7a624eb350e23bc215b42738ee4ba6e8d
GaborZeller <gabriel.d.celery@gmail.com>
2026-03-07 18:11:10 +0000

    fix: render-markdown got updated

--- .config/nvim/lua/plugins/styling/render-markdown.lua
+++ .config/nvim/lua/plugins/styling/render-markdown.lua
@@ -2,10 +2,14 @@ return {
   'MeanderingProgrammer/render-markdown.nvim',
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
+    'tree-sitter-grammars/tree-sitter-markdown', -- markdown parser for tree-sitter
+    'tree-sitter/tree-sitter-html', -- html grammar for tree-sitter
+    -- 'latex-lsp/tree-sitter-latex', -- don't need latex so disabling
+    'tree-sitter-grammars/tree-sitter-yaml', -- yaml grammar for tree-sitter
+    'nvim-tree/nvim-web-devicons', -- provides nerd font icons
+    'nvim-mini/mini.nvim', -- to get mini icons https://github.com/nvim-mini/mini.nvim/blob/main/readmes/mini-icons.md
+    'hrsh7th/nvim-cmp', -- for enabling completions
   },
-  -- 'echasnovski/mini.nvim', -- if you use the mini.nvim suite
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
   config = function()
     require('render-markdown').setup {
       heading = {
@@ -21,6 +25,8 @@ return {
         width = 'block',
       },
       latex = { enabled = false },
+      completions = { lsp = { enabled = true } },
+      pipe_table = { preset = 'heavy' },
     }
   end,
 }
