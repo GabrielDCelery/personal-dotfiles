72a26cf22e69b46eaeef90da69334c58092c0d86
GaborZeller <gabriel.d.celery@gmail.com>
2026-03-08 22:09:01 +0000

    feat: added snacks

---
+++ .config/nvim/lua/plugins/styling/snacks.lua
@@ -0,0 +1,27 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    animate = { enabled = true },
+    -- bigfile = { enabled = true },
+    -- dashboard = { enabled = true },
+    -- explorer = { enabled = true },
+    indent = {
+      enabled = true,
+    },
+    -- image = { enabled = true }, -- kitty support is limited (no zellij etc...) so diabled
+    input = { enabled = true },
+    -- picker = { enabled = true },
+    notifier = { enabled = true },
+    notify = { enabled = true },
+    -- quickfile = { enabled = true },
+    -- scope = { enabled = true },
+    -- scroll = { enabled = true },
+    -- statuscolumn = { enabled = true },
+    -- words = { enabled = true }, -- I got my own version of highlighting in my lspconfig.lua
+  },
+}
