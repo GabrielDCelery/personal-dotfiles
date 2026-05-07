366ee8b0a692dc0121b4d3a86b9b920164b8e767
GaborZeller <gabriel.d.celery@gmail.com>
2026-05-07 21:01:08 +0100

    feat: enabled other navigation otptions with flash

--- .config/nvim/lua/plugins/navigation/flash.lua
+++ .config/nvim/lua/plugins/navigation/flash.lua
@@ -1,7 +1,17 @@
 return {
   'folke/flash.nvim',
   event = 'VeryLazy',
-  opts = {},
+  opts = {
+    modes = {
+      treesitter = {
+        backdrop = 0.7,
+      },
+    },
+  },
+  config = function(_, opts)
+    require('flash').setup(opts)
+    vim.api.nvim_set_hl(0, 'FlashLabel', { bg = '#00ffff', fg = '#000000', bold = true })
+  end,
   keys = {
     {
       's',
@@ -11,30 +21,30 @@ return {
       end,
       desc = 'Flash',
     },
-    -- {
-    --   'S',
-    --   mode = { 'n', 'x', 'o' },
-    --   function()
-    --     require('flash').treesitter()
-    --   end,
-    --   desc = 'Flash Treesitter',
-    -- },
-    -- {
-    --   'r',
-    --   mode = 'o',
-    --   function()
-    --     require('flash').remote()
-    --   end,
-    --   desc = 'Remote Flash',
-    -- },
-    -- {
-    --   'R',
-    --   mode = { 'o', 'x' },
-    --   function()
-    --     require('flash').treesitter_search()
-    --   end,
-    --   desc = 'Treesitter Search',
-    -- },
+    {
+      'S',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').treesitter()
+      end,
+      desc = 'Flash Treesitter',
+    },
+    {
+      'r',
+      mode = 'o',
+      function()
+        require('flash').remote()
+      end,
+      desc = 'Remote Flash',
+    },
+    {
+      'R',
+      mode = { 'o', 'x' },
+      function()
+        require('flash').treesitter_search()
+      end,
+      desc = 'Treesitter Search',
+    },
     -- {
     --   '<c-s>',
     --   mode = { 'c' },
