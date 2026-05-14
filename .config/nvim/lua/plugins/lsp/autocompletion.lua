b3cc606ce9d31be02744eaa9f7dd55124d317531
GaborZeller <gabriel.d.celery@gmail.com>
2026-05-14 23:24:52 +0100

    feat: removed window setting in autocompletion because we are doing it via noice

--- .config/nvim/lua/plugins/lsp/autocompletion.lua
+++ .config/nvim/lua/plugins/lsp/autocompletion.lua
@@ -44,10 +44,6 @@ return { -- Autocompletion
       },
       -- completion = { completeopt = 'menu,menuone,preview,noselect' },
       completion = { completeopt = 'menu,menuone,noinsert' },
-      -- window = {
-      --   completion = cmp.config.window.bordered(),
-      --   documentation = cmp.config.window.bordered(),
-      -- },
 
       -- For an understanding of why these mappings were
       -- chosen, you will need to read `:help ins-completion`
